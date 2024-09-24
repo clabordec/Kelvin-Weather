f1faf8da8c706da61c79eeaaf324c6bc002486ba
Chaanyah Laborde <72324462+clabordec@users.noreply.github.com>
2024-09-24 01:54:17 -0400

    Update kelvin.js

--- kelvin.js
+++ kelvin.js
@@ -4,17 +4,20 @@ const kelvin = 0;
 // Celsius is 273 degrees less than Kelvin
 const celsius = kelvin - 273;
 
-// Calculate the Fahrenheit
-// Fahrenheit = Celsius * (9/5) + 32
+// Convert Fahrenheit to Celsius
 let fahrenheit = celsius * (9/5) + 32
+
+// Round Down
 fahrenheit = Math.floor(fahrenheit)
 
 // Display the results
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
 
-// Calculate Newton
-// Convert celsius to Newton
+// Convert Celsius to Newton
 let newton = celsius * (33/100);
+
+// Round Down
 newton = Math.floor(newton);
 
+// Display the output
 console.log(`The temperature is ${newton} degrees Newton.`)
